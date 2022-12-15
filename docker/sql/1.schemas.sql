323dbf28430bdecda52e360904edac74d625c7a2
EnzoZa <71082476+EnzoZa@users.noreply.github.com>
2022-12-15 16:07:29 +0100

    :sparkles::card_file_box: Init database

--- docker/sql/1.schemas.sql
+++ docker/sql/1.schemas.sql
@@ -1,3 +1,20 @@
 DROP SCHEMA IF EXISTS `hackathon`;
+
 CREATE SCHEMA IF NOT EXISTS `hackathon` DEFAULT CHARACTER SET utf8;
 USE `hackathon`;
+
+create table categorie
+(
+    id             int auto_increment,
+    categorie_name varchar(255) null,
+    constraint theme_id_uindex
+        unique (id)
+);
+
+create table question
+(
+    id       int auto_increment
+        primary key,
+    question varchar(255) null,
+    `order`  int          null
+);
---
+++ docker/sql/2.insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (1, 'Sport');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (2, 'Évènementiel');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (3, 'Écologie');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (4, 'Informatique');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (5, 'Vêtement');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (6, 'Touriste');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (7, 'Blog');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (8, 'Personnel');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (9, 'Religion');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (10, 'Touristique');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (11, 'Cours');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (12, 'Créateur de contenu');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (13, 'Vente');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (14, 'Association');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (15, 'Restaurant');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (16, 'Radio');
+INSERT INTO hackathon.categorie (id, categorie_name) VALUES (17, 'Épicerie');
+
+INSERT INTO hackathon.question (id, question, `order`) VALUES (1, 'Donner un mot clé pour décrire votre besoin ?', 1);
+INSERT INTO hackathon.question (id, question, `order`) VALUES (2, 'Quelle type de site web souhaitez vous avoir ?', 2);
+
